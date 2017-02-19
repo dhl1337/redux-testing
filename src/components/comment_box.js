eb002757bef141ba6ad37386e0768daa901a3d6a
Dan Le <dhl1337@gmail.com>
2017-02-18 18:49:01 -0700

    test class name

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,2 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
+class CommentBox extends Component {
+  render() {
+    return (
+      <div className="comment-box">
+        <textarea></textarea>
+        <button>Submit Comment</button>
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
