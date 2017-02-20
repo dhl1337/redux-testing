8772674642cbc5806ba252b35954d38454e2502e
Dan Le <dhl1337@gmail.com>
2017-02-19 18:24:12 -0700

    add testing library

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,11 +3,19 @@
 // build helper for simulating events
 // set up chai-jquery
 
-import jsdom from 'jsdom';
+import { jsdom } from 'jsdom';
 import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 
 //window -> global
-global.document = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
+global.document = jsdom('<!DOCTYPE html><html><body></body></html>');
 global.window = global.document.defaultView;
 
 const $ = jquery(global.window);
+
+function renderComponent(ComponentClass) {
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass/>);
+
+  ReactDOM.findDOMNode(componentInstance);
+}
