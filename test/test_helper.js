41007e215b072c62be6fbdaa2da4b39b1596dce9
Dan Le <dhl1337@gmail.com>
2017-02-19 19:06:04 -0700

    fix test helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,21 +2,24 @@
 // build 'renderComponent' helper that should render a given react class
 // build helper for simulating events
 // set up chai-jquery
+import jquery from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { jsdom } from 'jsdom';
-import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils';
+import jsdom from 'jsdom';
 import chai, { expect } from 'chai';
+import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
 //window -> global
-global.document = jsdom('<!DOCTYPE html><html><body></body></html>');
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
+global.navigator = global.window.navigator;
+const $ = jquery(window);
 
-const $ = jquery(global.window);
+chaiJquery(chai, chai.util, $);
 
 function renderComponent(ComponentClass, props, state) {
   const componentInstance = TestUtils.renderIntoDocument(
@@ -29,6 +32,10 @@ function renderComponent(ComponentClass, props, state) {
 }
 
 $.fn.simulate = function (eventName, value) {
+  if (value) {
+    this.val(value);
+  }
+
   TestUtils.Simulate[eventName](this[0]);
 };
 
